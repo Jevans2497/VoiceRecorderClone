2945f2878f5bc7a6cf431ca4a1457e3b224c6381
Jon Evans <Jevans2497@gmail.com>
2019-05-03 16:09:02 -0500

    Initial Commit

---
+++ VoiceRecorderClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VoiceRecorderClone
+//
+//  Created by Jonathan Evans on 5/3/19.
+//  Copyright © 2019 Jon Evans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ VoiceRecorderCloneTests/VoiceRecorderCloneTests.swift
@@ -0,0 +1,34 @@
+//
+//  VoiceRecorderCloneTests.swift
+//  VoiceRecorderCloneTests
+//
+//  Created by Jonathan Evans on 5/3/19.
+//  Copyright © 2019 Jon Evans. All rights reserved.
+//
+
+import XCTest
+@testable import VoiceRecorderClone
+
+class VoiceRecorderCloneTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
